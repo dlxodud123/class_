02974f612c4184fb2fc8c2711bc6a78e36e1070f
taeyoung <xodud5080@naver.com>
2024-02-15 17:46:00 +0900

    asssa

--- src/class_/QuizMainClass04.java
+++ src/class_/QuizMainClass04.java
@@ -1,24 +1,12 @@
 package class_;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class QuizMainClass04 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("수 입력 : ");
-		int num = sc.nextInt();
-		ArrayList<Integer> arr = new ArrayList<>();
-
-		while (true) { 
-			int temp = num % 10; 
-			int asdf = num / 10; 
-			arr.add(temp); 
-			if (asdf < 10) {
-				break;
-			}
-			num = asdf; 
-		}
-		System.out.println(arr);
+		QuizTestClass04 q = new QuizTestClass04();
+		int num = q.input();
+		ArrayList<Integer> arr = q.circul(num);
+		q.output(arr);
 	}
 }
--- src/class_/QuizTestClass04.java
+++ src/class_/QuizTestClass04.java
@@ -1,5 +1,36 @@
 package class_;
 
-public class QuizTestClass04 {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class QuizTestClass04 {
+	public int input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("수 입력 : ");
+		int num = sc.nextInt();
+		return num;
+	}
+	public ArrayList<Integer> circul(int num) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		while (true) { 
+			int temp = num % 10; 
+			int asdf = num / 10; 
+			arr.add(temp); 
+			if (asdf < 10) {
+				if (asdf == 0) {
+					break;
+				}
+				arr.add(asdf);
+				break;
+			}
+			num = asdf; 
+		}
+		return arr;
+	}
+	public void output(ArrayList<Integer> arr) {
+		for (int i = 0; i < arr.size(); i++) {
+			String s = arr.get(i) + "";
+			System.out.print(s);
+		}
+	}
 }
